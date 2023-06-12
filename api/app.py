2890b7673d3a2635b3e094a14d693b5c72e5b200
Harshit Singh <77427143+harshiitsingh@users.noreply.github.com>
2023-06-12 23:09:00 +0530

    updated app.py

--- api/app.py
+++ api/app.py
@@ -8,11 +8,7 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "*"
-]
+origins = ["http://localhost", "http://localhost:3000", "*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -21,13 +17,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MODEL = tf.keras.models.load_model("New folder\\api\\models\\potato-disease.h5")
+MODEL = tf.keras.models.load_model("potato-disease.h5")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
 
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
+
 
 def read_file_as_image(data) -> np.ndarray:
     # image = np.array(Image.open(BytesIO(data)))
@@ -37,22 +35,18 @@ def read_file_as_image(data) -> np.ndarray:
     # image = np.array(image) / 255.0  # Normalize image pixels
     return image
 
+
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
+async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    
+
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
-    return {
-        'class': predicted_class,
-        'confidence': float(confidence)
-    }
+    return {"class": predicted_class, "confidence": float(confidence)}
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000) # 8000 port is not working
-
+    uvicorn.run(app, host="localhost", port=8000)  # 8000 port is not working
